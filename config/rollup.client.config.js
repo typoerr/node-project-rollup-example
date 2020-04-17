a65d2cc1f34f98fbc5b0d1ec23b4d727e231f666
typoerr <masahirokato.dev@gmail.com>
2020-04-17 15:42:07 +0900

    chore: initial commit

---
+++ config/rollup.client.config.js
@@ -0,0 +1,37 @@
+import * as path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+
+const ENV = process.env.NODE_ENV || 'development'
+const IS_PROD = ENV === 'production'
+const ROOT = path.resolve(__dirname, '../')
+const BASE = path.join(ROOT, 'projects/client')
+const INPUT = path.join(BASE, 'index.ts')
+const OUTPUT = path.join(ROOT, 'dist/client')
+const TS_CONFIG = path.join(ROOT, 'tsconfig.json')
+
+export default {
+  context: 'window',
+  input: INPUT,
+  output: {
+    dir: OUTPUT,
+    format: 'iife',
+    sourcemap: true,
+    plugins: IS_PROD ? [terser()] : [],
+  },
+  plugins: [
+    typescript({
+      tsconfig: TS_CONFIG,
+      module: 'ESNext',
+      exclude: ['**/node_modules/**', '**/*.test.ts'],
+    }),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    resolve(),
+  ],
+}
---
+++ config/rollup.server.config.js
@@ -0,0 +1,40 @@
+import * as path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+
+const ROOT = path.resolve(__dirname, '../')
+const BASE = path.join(ROOT, 'projects/server')
+const INPUT = path.join(BASE, 'index.ts')
+const OUTPUT = path.join(ROOT, 'dist/server')
+const TS_CONFIG = path.join(ROOT, 'tsconfig.json')
+
+const external = (() => {
+  const dependencies = require(path.join(BASE, 'package.json')).dependencies || {}
+  const keys = ['@shared', ...Object.keys(dependencies)].map((key) => new RegExp(`^${key}`))
+  return (id) => keys.some((key) => key.test(id))
+})()
+
+export default {
+  context: 'this',
+  input: INPUT,
+  output: {
+    dir: OUTPUT,
+    format: 'cjs',
+    sourcemap: true,
+  },
+  plugins: [
+    typescript({
+      tsconfig: TS_CONFIG,
+      module: 'ESNext',
+      exclude: ['**/node_modules/**', '**/*.test.ts'],
+    }),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    resolve(),
+  ],
+  external,
+}
